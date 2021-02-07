ca62840b38fad8880dfe0bfb7a66601b97674d6b
= <takashizono@gmail.com>
2021-02-07 13:52:37 -0600

    updated git and api assignment

--- API_Ajax/pokemon/information_bootstrap/static/js/index.js
+++ API_Ajax/pokemon/information_bootstrap/static/js/index.js
@@ -1,40 +1,55 @@
 'use strict';
 
-// var list = document.getElementById('list');
-
 pokemon.forEach(p => {
-  // console.log(p.name);  
-  var section = document.createElement('section');
-  document.querySelector('main').appendChild(section);
+    
+  if(p.id == 1) {
+    var section = document.getElementById('place-1')
+  }
+  if(p.id == 2) {
+    var section = document.getElementById('place-2')
+  }
+  if(p.id == 3) {
+    var section = document.getElementById('place-3')
+  }
+  if(p.id == 4) {
+    var section = document.getElementById('place-4')
+  }
+  if(p.id == 5) {
+    var section = document.getElementById('place-5')
+  }
+  
+  var img = document.createElement('img');
+  img.src = p.image;
+  section.appendChild(img);
   
   var name = document.createElement('h3');
   name.innerHTML = p.name;
   section.appendChild(name);
   
-  var id_label = document.createElement('label');
-  id_label.innerHTML = 'Id:'
-  section.appendChild(id_label);
-  var id = document.createElement('p');
-  id.innerHTML = p.id;
-  id.classList.add('id');
-  id_label.appendChild(id);
+  // var id_label = document.createElement('label');
+  // id_label.innerHTML = 'Id:'
+  // section.appendChild(id_label);
+  // var id = document.createElement('p');
+  // id.innerHTML = p.id;
+  // id.classList.add('id');
+  // id_label.appendChild(id);
   
-  var height_label = document.createElement('label');
-  height_label.innerHTML = 'Height:'
-  section.appendChild(height_label);
-  var height = document.createElement('p');
-  height.innerHTML = p.height;
-  height.classList.add('height');
-  height_label.appendChild(height);
+  // var height_label = document.createElement('label');
+  // height_label.innerHTML = 'Height:'
+  // section.appendChild(height_label);
+  // var height = document.createElement('p');
+  // height.innerHTML = p.height;
+  // height.classList.add('height');
+  // height_label.appendChild(height);
   
-  var weight_label = document.createElement('label');
-  weight_label.innerHTML = 'Weight:'
-  section.appendChild(weight_label);
-  var weight = document.createElement('p');
-  weight.innerHTML = p.weight;
-  weight.classList.add('weight');
-  weight_label.appendChild(weight);
-
+  // var weight_label = document.createElement('label');
+  // weight_label.innerHTML = 'Weight:'
+  // section.appendChild(weight_label);
+  // var weight = document.createElement('p');
+  // weight.innerHTML = p.weight;
+  // weight.classList.add('weight');
+  // weight_label.appendChild(weight);
+  
   var moveList = document.createElement('div');
   moveList.classList.add('move_list');
   section.appendChild(moveList);
@@ -47,6 +62,6 @@ pokemon.forEach(p => {
     var move_name = document.createElement('li');
     move_name.innerHTML = move;
     ul.appendChild(move_name);
-  });  
+  });
   
 });
